f4e7ce5f721f22fa37a7d9f19ceb1b5a0c8a0580
Loïc Tardy <lotardy@gmail.com>
2021-04-28 09:03:54 +0200

    comments added

--- calculatrice/calculatrice.py
+++ calculatrice/calculatrice.py
@@ -1,21 +1,21 @@
-def ask_user(sentence = "Saisir un chiffre"):
+def ask_user(sentence = "Saisir un chiffre"):   #CamelCase removed.
     choice = input(f"""{sentence}\n>""")
     return choice
 
-def addition(number):
+def addition(number):   #Uppercase removed
     list_numbers = []
     while number.isdigit():
         if number.isdigit():
             list_numbers.append(int(number))
             number = ask_user("Saisir un chiffre à additionner ou clicker sur '=' ")
-    result = sum(list_numbers)
+    result = sum(list_numbers)  #List summarized
     return result
 
 def soustraction(number):
     list_numbers = []
     while number.isdigit:
         if number.isdigit():
-            number = int(number)
+            number = int(number)    #Number transformed into integer
             list_numbers.append(number)
             number = ask_user("Saisir un chiffre à soustraire ou clicker sur '=' ")
             i = 0
@@ -25,7 +25,7 @@ def soustraction(number):
                     i += 1
                 else:
                     result = result - list_number
-        else:
+        else:   #Add a break to the loop
             break
     return result
 
@@ -33,23 +33,23 @@ def multiplication(number):
     list_numbers = []
     while number.isdigit():
         if number.isdigit():
-            number = int(number)
+            number = int(number)    #Number transformed into integer
             list_numbers.append(number)
         number = ask_user("Saisir un chiffre à multiplier ou clicker sur '=' ")
         i = 0
         for list_number, index in zip(list_numbers,list(range(len(list_numbers)))): # refactoriser
-            if i == 0:
+            if i == 0:      #0 changed from string to a number
                 result = list_number
                 i += 1
             else:
-                result = result * list_number
+                result = result * list_number   #Changed the operator from a (divide) / to a (multiply)*
     return result
 
 def division(number):
     list_numbers = []
     while number.isdigit():
         if number.isdigit():
-            number = int(number)
+            number = int(number)    #Number transformed into integer
             list_numbers.append(number)
         number = ask_user("Saisir un chiffre à diviser ou clicker sur '=' ")
         i = 0
@@ -58,7 +58,7 @@ def division(number):
                 result = list_number
                 i += 1
         else:
-            result = result / list_number
+            result = result / list_number   #Changed the operator from a (plus) + to a (divide) /
             result = int(result)
     return result
 
@@ -70,7 +70,7 @@ def display_interface():
     3. Multiplier Tape 3
     4. Diviser Tape 4""")
     while choice.isdigit():
-        choice = int(choice)
+        choice = int(choice)    #choice transformed into integer
         if choice == 1:
             choice = ask_user("Saisir un chiffre à ADDITIONNER ou clicker sur '=' ")
             result = addition(choice)
